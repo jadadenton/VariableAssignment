d1ed3278aeb5249e450da465af6e362802e5b5c9
jadadenton <70173346+jadadenton@users.noreply.github.com>
2020-08-31 16:24:08 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+	int x = 50;
+	float y = 24.99f;
+	String z = "Bobby Johnson";
+	System.out.println("Test Score intialized is: " + x);
+	System.out.println("Price tag: " + y);
+	System.out.println("Username: "+ z);
+
+    }
+}
